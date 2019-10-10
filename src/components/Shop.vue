e19a177ea07d018c6ec6dadb360fd22c8277ddcc
Thomas Nabord <thomas.nabord@prestashop.com>
2019-10-10 16:41:08 +0100

    Fix unit tests and remove query param when provided

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -40,10 +40,18 @@ export default {
     },
   },
   created() {
-    const payload = {
-      idmodule: this.$route.query.idmodule,
-      moduleNameToinstall: this.$route.query.module_name_toinstall,
-    };
+    const payload = {};
+    if (this.$route !== undefined && this.$route.query !== undefined) {
+      payload.idmodule = this.$route.query.idmodule;
+      payload.moduleNameToinstall = this.$route.query.module_name_toinstall;
+
+      // Clean URL once the param are taken in account
+      let query = Object.assign({}, this.$route.query);
+      delete query.idmodule;
+      delete query.module_name_toinstall;
+      this.$router.replace({ query });
+    }
+    
     this.$store.dispatch('requestMachine', payload);
   },
 };
